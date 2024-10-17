47ca9cb307406b981953d2c237a1865256b46928
João Gabriel <joao.g.nascimento17@aluno.senai.br>
2024-10-17 12:39:30 -0300

    Começando aula 18 e corrigindo atividade aula 15

---
+++ aula_18/atividade/script.js
@@ -0,0 +1,12 @@
+/* function verificaIdade(idade){
+   if (idade>=18) {
+    alert("Você é maior de idade")
+   } else {
+    alert("Você é menor de idade")
+   }
+} */
+
+function alteraBackGround(){
+    const body = document.querySelector("body")
+    body.style.backgroundColor = "#f2c2f5"
+}
---
+++ aula_18/script.js
@@ -0,0 +1,9 @@
+function calculaMedia(valor1, valor2){
+    const media = (valor1+valor2)/2
+    return alert(media)
+}
+
+calculaMedia(6, 10)
+calculaMedia(2, 12)
+calculaMedia(4, 2)
+calculaMedia(4, 4)
