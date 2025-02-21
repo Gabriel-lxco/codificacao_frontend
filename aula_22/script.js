e3c0197c506be73a3f949e6fe77533b0db893a7f
Gabriel Laco <gabriellacotrab@gmail.com>
2025-02-21 17:43:18 -0300

    Atualizando atividade aula_22

--- aula_22/script.js
+++ aula_22/script.js
@@ -1,14 +1,26 @@
 function contactUs(){
-    const firstName = document.querySelector("#firstName")
-    const lastName = document.querySelector("#lastName")
-    const email = document.querySelector("#email")
-    const message = document.querySelector("#message")
-    const btn = document.querySelector(".btn")
+    const firstName = document.querySelector("#firstName");
+    const lastName = document.querySelector("#lastName");
+    const email = document.querySelector("#email");
+    const message = document.querySelector("#message");
+    const btn = document.querySelector(".btn");
 
     const firstNameValue = document.querySelector("#firstName").value
     const lastNameValue = document.querySelector("#lastName").value
     const emailValue = document.querySelector("#email").value
     const messageValue = document.querySelector("#message").value
+
+    //Começo de código com inteligência artifical
+    const generalEnquiry = document.querySelector("#general-enquiry");
+    const supportRequest = document.querySelector("#support-request");
+    const queryTypeBox = document.querySelectorAll(".box-querytype");
+
+    if (!generalEnquiry.checked && !supportRequest.checked) {
+        queryTypeBox.forEach(box => box.classList.add("erro"));
+    } else {
+        queryTypeBox.forEach(box => box.classList.remove("erro"));
+    }
+    //Fim de código feito com inteligência artificial
 
     if (firstNameValue === "") {
         firstName.classList.add("erro")
